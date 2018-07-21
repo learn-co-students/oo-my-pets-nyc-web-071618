1f44ca59584c545b4e837ecbe33fed8200d1d904
cmonkey03 <russellpbrown@gmail.com>
2018-07-21 14:27:54 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
+
 class Dog
   # code goes here
+
+    attr_reader :name
+    attr_accessor :mood
+
+    @@all = []
+
+    def initialize(name)
+      @name = name
+      @@all << name
+      @mood = "nervous"
+    end
+
 end
